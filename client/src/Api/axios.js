af1f1b81fb7baf4a11bfe416af87bd4bc189b6d2
Natnael-web-tech <gelalchanatnael@gmail.com>
2025-04-03 12:25:51 -0400

    first commit

---
+++ client/src/Api/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// Check if the app is running in production or development mode
+const axiosInstance = axios.create({
+  baseURL: window.location.hostname === 'evangadiforum.natiweb.site'
+    ? 'https://evangadiforum.natiweb.site/api'  // Production backend
+    : 'http://localhost:2017/api',               // Local backend for development
+});
+
+export default axiosInstance;
