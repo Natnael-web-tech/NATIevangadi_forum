58340053d87d7811ef4e581bb71e094da7d0c0a0
Natnael-web-tech <gelalchanatnael@gmail.com>
2025-04-04 21:22:45 -0400

    render config

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Check if the app is running in production or development mode
 const axiosInstance = axios.create({
-  baseURL: "https://natievangadi-forumbackend.onrender.com/api", // Production backend
-  //  'http://localhost:2017/api',               // Local backend for development
+  baseURL: "https://natievangadi-forumbackend-lh6o.onrender.com/api", // Production backend
+  //  'http://localhost:2017/api'               // Local backend for development
 });
 
 export default axiosInstance;
